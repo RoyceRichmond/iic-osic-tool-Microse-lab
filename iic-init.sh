02fe50b74fe66484906485687e0a0600fb3a1791
Royce Richmond <miles-militis@hotmail.com>
2023-10-08 18:14:34 -0600

    modified the pdk_root
    the prior variab le was exported as
    
    export PDK_ROOT=/home/USER/pdk
    
    however the user changes from machine to machine, changing it to $HOME ensures it works across many machines and users

--- iic-init.sh
+++ iic-init.sh
@@ -4,7 +4,7 @@
 # Institute for Integrated Circuits
 # Johannes Kepler University Linz
 #
-export PDK_ROOT=/home/richmond/pdk
+export PDK_ROOT=$HOME/pdk
 export PDK=sky130B
 export STD_CELL_LIBRARY=sky130_fd_sc_hd
 cp -f $PDK_ROOT/$PDK/libs.tech/xschem/xschemrc $HOME/.xschem
